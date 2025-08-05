cc8a19fa8c435b7bc7a736f4353b9731a0994e64
Raushan Jaiswal <jraushan54@gmail.com>
2025-08-05 16:49:04 +0530

    Added number-to-word logic (if-else challenge) From HackerRank

--- Basics.cpp
+++ Basics.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 1 && n <= 9) {
+        string words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        cout << words[n - 1] << endl;
+    } else {
+        if (n % 2 == 0) cout << "even" << endl;
+        else cout << "odd" << endl;
+    }
+    return 0;
+}
 
